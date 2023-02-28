39f83bf83e07ed1c78d84dfc3d532aeebb2a4df8
JANDRESI-PC\jandr <jandresi@hotmail.com>
2023-02-27 23:48:25 -0500

    Login funcionando correctamente junto a sidebar

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -3,56 +3,41 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Usuario } from 'src/app/domain/usuario';
 import { LoginService } from 'src/app/service/login.service';
-import { SesionServiceService } from 'src/app/service/sesion-service.service';
 import { UsuarioService } from 'src/app/service/usuario.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  
-  u:Usuario=new Usuario(0,"","",0);
-  user:string="";
-  pass:string="";
-  c:number=0;
+  u: Usuario = new Usuario(0, '', '', 0);
+  user: string = '';
+  pass: string = '';
+  c: number = 0;
 
-
-  constructor(private router: Router,
-    private loginService:LoginService,
-    private sesionService:SesionServiceService,
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
     private snackBar: MatSnackBar,
-    private usuarioService:UsuarioService) { }
+    private usuarioService: UsuarioService
+  ) {}
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {
       duration: 5000,
     });
   }
+
   verificar() {
-    
-     
-  
-    this.loginService.login(this.user,this.pass).subscribe(
-
-      res => {
-        this.sesionService.setUserLoggedIn(this.user);
-          this.router.navigate(['index-menu']);
+    this.loginService.login(this.user, this.pass).subscribe(
+      (res) => {
+        this.loginService.setUserLoggedIn(this.user);
       },
-      error => {
-       
-        
-        this.openSnackBar(error.error.mensaje,"ERROR");
-        
-        }
-
-        );}
-
-
-  
-
+      (error) => {
+        this.openSnackBar(error.error.mensaje, 'ERROR');
+      }
+    );
+  }
 }
