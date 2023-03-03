223a17ddda7fab1f953f4618a5529001eb248964
JANDRESI-PC\jandr <jandresi@hotmail.com>
2023-03-02 23:33:47 -0500

    Formulario de registrar atencion medica o psicologica

--- src/app/component/Create/asistenciamedica-create/asistenciamedica-create.component.ts
+++ src/app/component/Create/asistenciamedica-create/asistenciamedica-create.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-asistenciamedica-create',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./asistenciamedica-create.component.scss']
 })
 export class AsistenciamedicaCreateComponent {
+  fechaHoy = moment().format('YYYY-MM-DD');
+  form = {
+    cedulaPaciente: '',
+    nombrePaciente: '',
+    fechaConsulta: this.fechaHoy,
+    diagnostico: '',
+    descripcion: '',
+    tratamiento: '',
+  }
 
+  constructor(private snackBar: MatSnackBar,) {}
+
+  ngOnInit() {
+  }
+
+  registrarEvento() {
+    return console.log(this.form);
+  }
+
+  limpiarCampos() {
+    this.form = {
+      cedulaPaciente: '',
+      nombrePaciente: '',
+      fechaConsulta: '',
+      diagnostico: '',
+      descripcion: '',
+      tratamiento: '',
+    }
+  }
 }
