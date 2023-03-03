684c16f3ef96599ff2ee69de011ff37d64dd217d
oswaldo305 <os-w316@hotmail.com>
2023-03-02 21:35:16 -0500

    Rol view

--- src/app/component/Create/rol-create/rol-create.component.ts
+++ src/app/component/Create/rol-create/rol-create.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-rol-create',
   templateUrl: './rol-create.component.html',
   styleUrls: ['./rol-create.component.scss']
 })
-export class RolCreateComponent {
+export class RolCreateComponent  implements OnInit{
 
+  nombreRol:string = '';
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any,
+    public dialogRef: MatDialogRef<RolCreateComponent>
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  crearRol() {
+    console.log(this.nombreRol);
+    this.cerrar();
+  }
+
+  cerrar() {
+    this.dialogRef.close();
+  }
 }
+
--- src/app/component/List/rol-list/rol-list.component.ts
+++ src/app/component/List/rol-list/rol-list.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { RolCreateComponent } from '../../Create/rol-create/rol-create.component';
 
 @Component({
   selector: 'app-rol-list',
   templateUrl: './rol-list.component.html',
   styleUrls: ['./rol-list.component.scss']
 })
-export class RolListComponent {
+export class RolListComponent implements OnInit, AfterViewInit{
+  public categorias = new MatTableDataSource<any>();
+  public displayedColumns: string[] = ['id', 'nombre', 'acciones'];
+  @ViewChild('paginator') paginator: any = MatPaginator;
+
+  constructor(public dialog: MatDialog) {}
+
+  ngAfterViewInit(): void {
+    this.categorias.paginator = this.paginator;
+  }
+
+  ngOnInit():void {
+    this.categorias.data = [
+      {id: 1, nombre:"Deportes"},
+      {id: 2, nombre:"Orquestas"},
+      {id: 3, nombre:"Gimnasio"},
+      {id: 4, nombre:"Danzas"},
+    ];
+  }
+
+  crearNuevoRol() {
+    let dialogRef = this.dialog.open(RolCreateComponent, {
+      width: '400px',
+      height: 'max-content',
+      autoFocus: false,
+      data: {}
+    });
+  }
 
 }
