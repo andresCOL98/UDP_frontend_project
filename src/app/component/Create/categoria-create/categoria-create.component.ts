fd98f46c8d69d838d5df97199131290f8d1c447f
oswaldo305 <os-w316@hotmail.com>
2023-03-08 15:45:37 -0500

    validador de forget-data!

--- src/app/component/Create/categoria-create/categoria-create.component.ts
+++ src/app/component/Create/categoria-create/categoria-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Categoria } from 'src/app/domain/categoria';
+import { CategoriaService } from 'src/app/service/categoria.service';
 
 @Component({
   selector: 'app-categoria-create',
@@ -9,16 +12,35 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class CategoriaCreateComponent implements OnInit {
   nombreCategoria:string = '';
 
+  categoria:Categoria=new Categoria(0,false,"");
+  showMsg:boolean=false;
+  msj:string="";
+
   constructor(@Inject(MAT_DIALOG_DATA) public data:any,
-    public dialogRef: MatDialogRef<CategoriaCreateComponent>
+    public dialogRef: MatDialogRef<CategoriaCreateComponent>,
+    public matSnackBar:MatSnackBar,
+    public categoriaService:CategoriaService
   ) { }
 
   ngOnInit(): void {
+    this.categoria=new Categoria(0,false,"");
+  }
+
+  public openSnackBar(mesagge:string,action:string){
+    this.matSnackBar.open(mesagge,action,{duration:3000});
   }
 
   crearCategoria() {
-    console.log(this.nombreCategoria);
-    this.cerrar();
+    this.categoriaService.createCategoria(this.categoria).subscribe(data=>{
+     
+      this.openSnackBar('Categoría creada exitosamente','Exito');
+      this.cerrar();
+    },error=>{
+      this.openSnackBar(error.error.mensaje,'Error');
+    }
+
+    );
+  
   }
 
   cerrar() {
--- src/app/component/forget-data/forget-data.component.ts
+++ src/app/component/forget-data/forget-data.component.ts
@@ -1,5 +1,6 @@
 import { Component,Inject, OnInit } from '@angular/core';
 import { MatDialogRef,MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-forget-data',
@@ -10,18 +11,39 @@ export class ForgetDataComponent implements OnInit{
   nombre:string='';
   cedula:any=null;
   detalle:string='';
+  varidador:boolean=false;
  
 
   constructor(@Inject(MAT_DIALOG_DATA) public data:any,
-    public dialogRef: MatDialogRef<ForgetDataComponent>
+    public dialogRef: MatDialogRef<ForgetDataComponent>,
+    public matSnackBar:MatSnackBar
   ) { }
 
   ngOnInit() {
   }
 
+  public openSnackBar(mesagge:string,action:string){
+    this.matSnackBar.open(mesagge,action,{duration:3000});
+  }
+
+  validarForm(){
+    if(this.nombre=='' || this.detalle=='' || this.cedula==null){
+    }else{
+      this.varidador=true;
+    }
+
+    return this.varidador;
+  }
+
   enviarForm() {
-    window.open('mailto:os-w316@hotmail.com?Subject=Recuperar%20cuenta%20UDP&body=Hola perdí el acceso a mi cuenta. NOMBRE:'+this.nombre+', CEDULA:'+this.cedula+', DETALLE:'+this.detalle,'_blank');
-    this.cerrar();
+
+    this.validarForm();
+    if(this.varidador==true){
+      window.open('mailto:os-w316@hotmail.com?Subject=Recuperar%20cuenta%20UDP&body=Hola perdí el acceso a mi cuenta. NOMBRE:'+this.nombre+', CEDULA:'+this.cedula+', DETALLE:'+this.detalle,'_blank');
+      this.cerrar();
+    }else{
+      this.openSnackBar('Verifique los campos','Error');
+    }
   }
 
   cerrar() {
