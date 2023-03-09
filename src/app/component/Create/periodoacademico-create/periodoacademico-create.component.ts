cd9b9949b6407ba1403dc0341e39bdc348a2f7b0
oswaldo305 <os-w316@hotmail.com>
2023-03-09 15:48:49 -0500

    Fix errores + PeriodoAcademico FUNCIONANDO

--- src/app/component/Create/periodoacademico-create/periodoacademico-create.component.ts
+++ src/app/component/Create/periodoacademico-create/periodoacademico-create.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoadingService } from 'src/app/service/loading.service';
+import { PeriodoacademicoService } from 'src/app/service/periodoacademico.service';
 
 @Component({
   selector: 'app-periodoacademico-create',
   templateUrl: './periodoacademico-create.component.html',
   styleUrls: ['./periodoacademico-create.component.scss']
 })
-export class PeriodoacademicoCreateComponent {
+export class PeriodoacademicoCreateComponent implements OnInit{
 
+  nombrePeriodo:number = 0;
+  anio:number=0;
+  estado:boolean=true;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any,
+    public dialogRef: MatDialogRef<PeriodoacademicoCreateComponent>,
+    private periodoService: PeriodoacademicoService,
+    private snackBar: MatSnackBar,
+    private loading: LoadingService
+  ) { }
+
+  ngOnInit(): void {
+    this.nombrePeriodo = this.data.periodo || '';
+    this.anio=this.data.anio || '';
+  }
+
+  crearPeriodo() {
+    if(this.nombrePeriodo < 0 || this.nombrePeriodo > 3) return this.snackBar.open('Inserte un período válido', undefined, {duration: 3000});
+    if(this.anio < 2023) return this.snackBar.open('Inserte un año válido', undefined, {duration: 3000});
+
+    let datosPeriodo={
+      id: this.data.id,
+      periodo: this.nombrePeriodo,
+      anio:this.anio,
+      activo:true
+
+    };
+
+    this.loading.cargando.next(true);
+    this.periodoService.createPeriodo(datosPeriodo).subscribe(res => {
+      this.snackBar.open('Creado exitosamente', undefined, {duration: 3000});
+      this.dialogRef.close(true);
+    },(error) => {
+      this.snackBar.open('Ha fallado la creación del período', undefined, {duration: 3000});
+    })
+    return this.loading.cargando.next(false);
+  }
+
+  editarPeriodo() {
+    if(this.nombrePeriodo < 0 || this.nombrePeriodo > 3) return this.snackBar.open('Inserte un período válido', undefined, {duration: 3000});
+    if(this.anio < 2023) return this.snackBar.open('Inserte un año válido', undefined, {duration: 3000});
+    let datosPeriodo={
+      id: this.data.id,
+      periodo: this.nombrePeriodo,
+      anio:this.anio,
+      activo:this.data.activo
+
+    };
+    this.loading.cargando.next(true);
+
+    this.periodoService.updatePeriodo(datosPeriodo).subscribe(res => {
+      this.snackBar.open('Actualizado exitosamente', undefined, {duration: 3000});
+      this.dialogRef.close(true);
+    },(error) => {
+      this.snackBar.open('Ha fallado la actualización del período', undefined, {duration: 3000});
+    })
+    return this.loading.cargando.next(false);
+  }
 }
+
--- src/app/component/List/periodoacademico-list/periodoacademico-list.component.ts
+++ src/app/component/List/periodoacademico-list/periodoacademico-list.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import {  AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Periodoacademico } from 'src/app/domain/periodoacademico';
+import { LoadingService } from 'src/app/service/loading.service';
+import { PeriodoacademicoService } from 'src/app/service/periodoacademico.service';
+import { PeriodoacademicoCreateComponent } from '../../Create/periodoacademico-create/periodoacademico-create.component';
 
 @Component({
   selector: 'app-periodoacademico-list',
   templateUrl: './periodoacademico-list.component.html',
   styleUrls: ['./periodoacademico-list.component.scss']
 })
-export class PeriodoacademicoListComponent {
+export class PeriodoacademicoListComponent implements  OnInit, AfterViewInit{
+  public periodos = new MatTableDataSource<any>();
+  public displayedColumns: string[] = ['id', 'periodo', 'anio', 'acciones'];
+  @ViewChild('paginator') paginator:any = MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort:any = MatSort;
+  activos:boolean = true;
+
+  constructor(public dialog: MatDialog,
+    private periodoService:PeriodoacademicoService,
+    private loading:LoadingService,
+    private snackBar: MatSnackBar) {}
+
+  ngAfterViewInit(): void {
+    this.periodos.paginator = this.paginator;
+    this.periodos.sort = this.sort
+  }
+
+  ngOnInit():void {
+    this.traerPeriodos();
+  }
+
+  traerPeriodos() {
+    this.loading.cargando.next(true);
+    this.periodoService.getPeriodosAcademicos(this.activos).subscribe((res:any) => {
+      this.periodos.data = res;
+    }, (error) => {
+      this.snackBar.open('Error al traer los datos de la tabla', undefined, {duration: 4000});
+    });
+    this.loading.cargando.next(false);
+  }
+
+  crearNuevoPeriodo() {
+    let dialogRef = this.dialog.open(PeriodoacademicoCreateComponent, {
+      width: '400px',
+      height: 'max-content',
+      autoFocus: false,
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(res => {
+      if(res) this.traerPeriodos();
+    })
+  }
+
+  editarPeriodo(periodo:Periodoacademico) {
+    let dialogRef = this.dialog.open(PeriodoacademicoCreateComponent, {
+      width: '400px',
+      height: 'max-content',
+      autoFocus: false,
+      data: periodo
+    });
+
+    dialogRef.afterClosed().subscribe(res => {
+      if(res) this.traerPeriodos();
+    })
+  }
 
 }
+
+
